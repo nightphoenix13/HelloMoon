71fe4e493dc7f6a9a5b6daaae27e99080c130130
Zack Phoenix <nightphoenix13@hotmail.com>
2014-07-29 14:07:30 -0500

    Updated up to chapter 14

--- src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -13,6 +13,13 @@ public class HelloMoonFragment extends Fragment
 	private Button mPauseButton;
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+	
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
 			Bundle savedInstanceState)
 	{
